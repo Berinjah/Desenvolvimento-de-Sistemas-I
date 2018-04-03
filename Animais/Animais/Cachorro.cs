ab6634ab2ae9dff02ab9abadb615ffe0fc2dedf9
Berinjah <lucas.silva2808@etec.sp.gov.br>
2018-04-03 20:34:53 -0300

    Exercicio de override e overload

--- Animais/Animais/Cachorro.cs
+++ Animais/Animais/Cachorro.cs
@@ -15,5 +15,31 @@ namespace Animais
         {
             Console.WriteLine("Abana o rabo");
         }
+        public string reagir(String atitude){
+            if (atitude == "Brincar" && atitude == "Acariciar"){
+                return abanarRabo;
+            }
+            else{
+                Console.WriteLine("Rosna");
+            }
+        public int reagir(int hora, min){
+            if(hora<12){
+                Console.WriteLine("Abana o rabo e late");
+            }
+            else if(hora>=18){
+                Console.WriteLine("Não liga para o dono");
+            }
+            else{
+                return abanarRabo;
+            }
+        }
+        public Boolean reagir(Boolean dono){
+            if (done == true){
+                Console.WriteLine("Abana o rabo e brinca");
+            }
+            else{
+                Console.WriteLine("Avança e rosna");
+            }
+        }
     }
 }
